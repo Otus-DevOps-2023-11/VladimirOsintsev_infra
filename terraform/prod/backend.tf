7ede02bfcfb5f9a8d47b8628e68e62a3e9483bb0
Vladimir.Osintsev <vladimir_tmail@mail.ru>
2023-09-24 22:42:03 +0700

    Fix 1

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -5,13 +5,14 @@
 #    }
 #  }
 #  required_version = ">= 0.13"
-#    backend "s3" {
-#     endpoint   = "storage.yandexcloud.net"
-#     bucket     = "s3terraform"
-#     region     = "ru-central1"
-#     key        = "prod.tfstate"
-#
-#     skip_region_validation      = true
-#     skip_credentials_validation = true
-#  }
-#}
+terraform {
+    backend "s3" {
+     endpoint   = "storage.yandexcloud.net"
+     bucket     = "s3terraform"
+     region     = "ru-central1"
+     key        = "prod.tfstate"
+
+     skip_region_validation      = true
+     skip_credentials_validation = true
+  }
+}
