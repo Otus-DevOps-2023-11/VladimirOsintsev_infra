4044d9effcbe37c19dbfc1bb693ae311bcb9650f
Vladimir Osintsev <vladimir_tmail@mail.ru>
2023-10-31 21:50:35 +0700

    Ansible-2 first

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -8,11 +8,12 @@ terraform {
 #terraform {
     backend "s3" {
      endpoint   = "storage.yandexcloud.net"
-     bucket     = "s3terraform"
+     bucket     = "s3terraform1"
      region     = "ru-central1"
      key        = "prod.tfstate"
 
      skip_region_validation      = true
      skip_credentials_validation = true
+     skip_requesting_account_id  = true
   }
 }
